40e4b1dd19fb175f06e42f07c3afa093256a294d
Diogo Dantas goncalves <ddantas-@c2r5s2.42lisboa.com>
2023-01-27 16:36:05 +0000

    adicionar free lst na main

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 18:47:35 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/01/27 16:26:28 by ddantas-         ###   ########.fr       */
+/*   Updated: 2023/01/27 16:35:48 by ddantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,17 @@ int	main(int argc, char **argv)
 	lst = (t_alist *)malloc(sizeof(t_alist));
 	if (!lst)
 		free(lst);
-	lst->data = 1;
-	lst->next = NULL;
 	ft_printf("list size = %d\n", ft_lstsize_2(lst));
 	ft_printf("lst.data = %d | lst.next = %p\n", lst->data, lst->next);
 	// List experiment End
 	
 	stack_a = (int *)malloc((argc - 1) * sizeof(int));
 	if (!stack_a)
-		free_stuff(stack_a, 0, 1);	
+		free_stuff(lst, stack_a, 0, 1);	
 	protection(lst, stack_a, argc, argv);
 	free(stack_a);
 
 	ft_printf("Ready to be sorted!\n");
-
-	/* 
-	// print Stack 
-	ft_printf("Stack: A\n");
-	int n = 0;
-	while (n < argc - 1)
-		ft_printf("%d\n", stack_a[n++]);
-	ft_printf("_\n");
-	// Print Stack End */
 	free(lst);
 	return (0);
 }
